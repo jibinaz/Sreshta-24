ef0a65a82a23a52fa136a7bd17a74862244a0c28
jibinaz <144548144+jibinaz@users.noreply.github.com>
2024-02-28 15:31:20 +0530

    scoreboard created
    scoreboard along with textbox added

--- app/components/arts/ArtMain.jsx
+++ app/components/arts/ArtMain.jsx
@@ -3,7 +3,7 @@ import ScoreBoard from "./ScoreBoard";
 
 const ArtMain = () => {
   return (
-    <div className="w-full bg-[#FFFDF6]">
+    <div className="w-full bg- [#FFFDF6]">
       <LandingVarnam />
       <ScoreBoard />
     </div>
--- app/components/arts/ScoreBoard.jsx
+++ app/components/arts/ScoreBoard.jsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
 import React from "react";
 
+
 const ScoreBoard = () => {
-  return <div className="w-screen"></div>;
+  return <div className="w-screen h-screen ">
+     <div className=" pl-28 pt-20 bangers text-6xl md:text-8xl  text-[#FFE179] HeroTextMainTwo">
+            score Board
+      </div>
+     <div className=" pl-28 pt-2 text-2xl md:text-3xl dog text-[#375E5E]">
+        Four teams, one trophy. The competition is<br />fair and square so that the win is deserved.
+      </div> 
+      <div className="relative w-1/2 h-16">
+      <Image             
+            src="\textbox.svg"
+            width={100}
+            height={100}
+            alt="Shreshta Logo"
+            className=" w-full h-full absolute top-0 left-0"
+      />
+
+      </div>
+
+  </div>;
+
 };
 
 export default ScoreBoard;
