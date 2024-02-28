4994a9f0a53b07c9572e144a5077bbbfc834889d
jibinaz <144548144+jibinaz@users.noreply.github.com>
2024-02-28 17:16:45 +0530

    scoreboard updated
    modification done

--- app/components/arts/ScoreBoard.jsx
+++ app/components/arts/ScoreBoard.jsx
@@ -10,15 +10,15 @@ const ScoreBoard = () => {
      <div className=" pl-28 pt-2 text-2xl md:text-3xl dog text-[#375E5E]">
         Four teams, one trophy. The competition is<br />fair and square so that the win is deserved.
       </div> 
-      <div className="relative w-1/2 h-16">
+      <div className="relative w-1/3 h-16 ml-16 mt-8 z-0 flex items-center ">
+        <p className="absolute top-[1px] ml-10 pt-4 dog text-3xl ">Green House won 2st in Music Mania +16 pts</p>
       <Image             
             src="\textbox.svg"
             width={100}
             height={100}
             alt="Shreshta Logo"
-            className=" w-full h-full absolute top-0 left-0"
+            className=" w-[734px] h-[78px] top-0 left-0"
       />
-
       </div>
 
   </div>;
